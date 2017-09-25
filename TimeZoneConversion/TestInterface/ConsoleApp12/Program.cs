a604467690e5aa083eb0ac6ad461aa88c4e20e0c
MDBarbier <matthew-7.7@hotmail.com>
2017-09-25 14:09:19 +0100

    Added TimeZoneConverter
    class library to convert a DateTime between timezones, local time and UTC in various ways.
    
    Takes a string input for the desired timezone (partial matching i.e. "Tallinn" will work)
    
    Also includes a console app to test the library.

---
+++ TimeZoneConversion/TestInterface/ConsoleApp12/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeZoneConverter;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This simulates a Scientia appointment from the View
+            DateTime originalTime = DateTime.Parse("2017/09/26 18:00:00");
+
+            //This code simulates converting to a Time zone based on the timezone field from canvas user profile
+            //This would be done in the scientia-canvas-sync, after comparison but before transmission over API
+            bool error1;
+            DateTime convertedToTallinnTime = TzConverter.ConvertToTargetTimeZoneFromLocalTime(originalTime, "Tallinn", out error1);
+
+            if (error1)
+            {
+                //remedial action
+            }
+
+            //These 2 lines are just converting to UTC which would happen when we DL the appointment from the Canvas API
+            TimeZoneInfo tz = TzConverter.GetTimeZoneInfo("Tallinn");
+            DateTime backToUtc = TimeZoneInfo.ConvertTimeToUtc(convertedToTallinnTime, tz);
+
+            //This is how we would convert the time downloaded from Canvas API before comparison/storing
+            DateTime local = TzConverter.ConvertFromUtcToLocalTime(backToUtc);
+
+            //This just shows how a time can be converted directly from target TZ to local time if needed
+            bool error2;
+            DateTime backToLocal = TzConverter.ConvertToLocalTimeFromTargetTimeZone(convertedToTallinnTime, "Tallinn", out error2);
+
+            if (error2)
+            {
+                //remedial action
+            }
+
+            //Logging
+            Console.WriteLine($"Converted output: {convertedToTallinnTime.ToString()}");
+            Console.WriteLine($"Converted back to UTC: {backToUtc}");
+            Console.WriteLine($"Converted to local time: {local}");
+            Console.WriteLine($"Converted straight from Tallinn time to UK time: {backToLocal}");
+            Console.WriteLine("Original input: " + originalTime.ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TimeZoneConversion/TimeZoneConverter/TzConverter.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TimeZoneConverter
+{
+    public class TzConverter
+    {
+        //To be used when you are confident that only one time zone will contain the string you send
+        public static DateTime ConvertToTargetTimeZoneFromLocalTime(DateTime toConvert, string sourceTimeZone, out bool error)
+        {
+            error = false;
+
+            DateTime utc = toConvert.ToUniversalTime();
+
+            IReadOnlyCollection<TimeZoneInfo> cstZonse = TimeZoneInfo.GetSystemTimeZones();
+
+            TimeZoneInfo tz = cstZonse.Where(a => a.DisplayName.Contains(sourceTimeZone)).FirstOrDefault();
+
+            if (tz == null)
+            {
+                error = true;
+                return new DateTime();
+            }
+            else
+            {
+                return TimeZoneInfo.ConvertTimeFromUtc(utc, tz);
+            }            
+        }
+
+        //To be used when you are confident that only one time zone will contain the string you send
+        public static DateTime ConvertToTargetTimeZoneFromUtcTime(DateTime toConvert, string sourceTimeZone, out bool error)
+        {
+            error = false;
+
+            IReadOnlyCollection<TimeZoneInfo> cstZonse = TimeZoneInfo.GetSystemTimeZones();
+
+            TimeZoneInfo tz = cstZonse.Where(a => a.DisplayName.Contains(sourceTimeZone)).FirstOrDefault();
+
+            if (tz == null)
+            {
+                error = true;
+                return new DateTime();
+            }
+            else
+            {
+                return TimeZoneInfo.ConvertTimeFromUtc(toConvert, tz);
+            }
+            
+        }
+
+        public static DateTime ConvertToLocalTimeFromTargetTimeZone(DateTime toConvert, string sourceTimeZone, out bool error)
+        {
+            error = false;
+
+            IReadOnlyCollection<TimeZoneInfo> cstZonse = TimeZoneInfo.GetSystemTimeZones();
+
+            TimeZoneInfo tz = cstZonse.Where(a => a.DisplayName.Contains(sourceTimeZone)).FirstOrDefault();
+
+            if (tz == null)
+            {
+                error = true;
+                return new DateTime();
+            }
+            else
+            {
+                DateTime utc = TimeZoneInfo.ConvertTimeToUtc(toConvert, tz);
+
+                return ConvertFromUtcToLocalTime(utc);
+            }            
+        }
+
+        //To be used when you need TimeZoneInfo object from a string
+        public static TimeZoneInfo GetTimeZoneInfo(string sourceTimeZone)
+        {
+            IReadOnlyCollection<TimeZoneInfo> cstZonse = TimeZoneInfo.GetSystemTimeZones();
+
+            TimeZoneInfo tz = cstZonse.Where(a => a.DisplayName.Contains(sourceTimeZone)).FirstOrDefault();
+
+            return tz;
+        }
+
+        //Takes a UTC datetime and converts to local time
+        public static DateTime ConvertFromUtcToLocalTime(DateTime toConvert)
+        {
+            DateTime kindSpecifiedDate = DateTime.SpecifyKind(toConvert, DateTimeKind.Utc);
+            DateTime convertedToLocal = kindSpecifiedDate.ToLocalTime();
+
+            return convertedToLocal;
+        }
+
+        //Takes a string representing a UTC datetime and converts to local time, our parameter specified if successful or not
+        public static DateTime ConvertStringFromUtcToLocalTime(string toConvert, out bool error)
+        {
+            error = false;
+
+            DateTime parsed = new DateTime();
+
+            try
+            {
+                parsed = DateTime.Parse(toConvert);
+            }
+            catch
+            {
+                error = true;
+                return new DateTime();
+            }
+
+            DateTime kindSpecifiedDate = DateTime.SpecifyKind(parsed, DateTimeKind.Utc);
+            DateTime convertedToLocal = kindSpecifiedDate.ToLocalTime();
+
+            return convertedToLocal;
+        }
+
+        //To be used when you are not sure that only one time zone will contain the string you send
+        public static string[] GetPotentialZones(string sourceTimeZone)
+        {
+            IReadOnlyCollection<TimeZoneInfo> cstZonse = TimeZoneInfo.GetSystemTimeZones();
+
+            List<TimeZoneInfo> tzs = cstZonse.Where(a => a.DisplayName.Contains(sourceTimeZone)).ToList();
+            List<string> names = new List<string>();
+
+            foreach (var tz in tzs)
+            {
+                names.Add(tz.DisplayName);
+            }
+
+            return names.ToArray();
+        }
+
+        //Simple conversion from local time to UTC
+        public static DateTime ConvertToUTC(DateTime toConvert)
+        {
+            DateTime utc = toConvert.ToUniversalTime();
+
+            return utc;
+        }
+    }
+}
