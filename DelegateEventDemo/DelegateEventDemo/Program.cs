0124eb4d9d6a86b056c63b755de606ad7dc3b598
matt barbier <matthewdavidbarbier@gmail.com>
2018-07-23 16:17:02 +0100

    Indexer demo

---
+++ DelegateEventDemo/DelegateEventDemo/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace EventSample
+{
+    public delegate void ContractHandler(Employee sender);
+
+    public class Employee
+    {
+        private bool bIsEngaged = false;
+        private int iAge = -1;
+        private String strName = null;
+        public event ContractHandler Engaged;
+
+        public Employee()
+        {
+            this.Engaged += new ContractHandler(this.OnEngaged);
+        }
+
+        private void OnEngaged(Employee sender)
+        {
+            Console.WriteLine("private void OnEngaged was called! this employee is engaged now!");
+        }
+
+        public string Name
+        {
+            get { return strName; }
+            set { strName = value; }
+        }
+
+        public int Age
+        {
+            get { return iAge; }
+            set { iAge = value; }
+        }
+
+        public bool IsEngaged
+        {
+            get { return bIsEngaged; }
+            set
+            {
+                if (bIsEngaged == false && value == true)
+                {
+                    Engaged(this);
+                }
+                bIsEngaged = value;
+            }
+        }
+    }
+
+    public class EntryPointClass
+    {
+        static void Main(string[] a_strArgs)
+        {
+            Employee simpleEmployee = new Employee();
+
+            simpleEmployee.Age = 18;
+            simpleEmployee.Name = "Samanta Rock";
+
+            simpleEmployee.Engaged += new ContractHandler(SimpleEmployee_Engaged);
+
+            //simpleEmployee.IsEngaged = true;
+
+            Console.ReadLine();
+
+            return;
+        }
+
+        static void SimpleEmployee_Engaged(Employee sender)
+        {
+            Console.WriteLine("The employee {0} is happy!", sender.Name);
+        }
+    }
+}
---
+++ IndexersDemo/IndexerTest/IndexerTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class SampleCollection<T>
+{
+    // Declare an array to store the data elements.
+    private T[] arr = new T[100];
+
+    // Define the indexer to allow client code to use [] notation.
+    public T this[int i]
+    {
+        get { return arr[i]; }
+        set { arr[i] = value; }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var stringCollection = new SampleCollection<string>();
+        stringCollection[0] = "Hello, World";
+        stringCollection[1] = "Greetings chummer";
+        Console.WriteLine(stringCollection[0]);
+        Console.WriteLine(stringCollection[1]);
+
+        var personCollection = new SampleCollection<Person>();
+        personCollection[0] = new Person() { Name = "Matt", Age = 36 };
+
+        Console.WriteLine($"{personCollection[0].Name}, {personCollection[0].Age}");
+
+        Console.ReadLine();
+    }
+}
+
+class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
