582c9ada3f5500567d57c80564783e815db4ff2f
MDBarbier <matthew-7.7@hotmail.com>
2018-11-19 21:35:46 +0000

    reactjsdotnetdemo

---
+++ ReactJsDotNet/Demo1/Demo1/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using Demo1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+
+namespace Demo1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly IList<CommentModel> _comments;
+
+        //constructor
+        static HomeController()
+        {
+            _comments = new List<CommentModel>
+            {
+                new CommentModel
+                {
+                    Id = 1,
+                    Author = "Daniel Lo Nigro",
+                    Text = "Hello ReactJS.NET World!"
+                },
+                new CommentModel
+                {
+                    Id = 2,
+                    Author = "Pete Hunt",
+                    Text = "This is one comment"
+                },
+                new CommentModel
+                {
+                    Id = 3,
+                    Author = "Jordan Walke",
+                    Text = "This is *another* comment"
+                },
+            };
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("comments")]
+        [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
+        public ActionResult Comments()
+        {
+            return Json(_comments);
+        }
+
+
+    }
+}
---
+++ ReactJsDotNet/Demo1/Demo1/wwwroot/js/tutorial.jsx
@@ -0,0 +1,92 @@
+﻿const data = [
+    { Id: 1, Author: 'Matt Barbier', Text: 'Title1' },
+    { Id: 2, Author: 'Matt Barbier', Text: 'Title2' },
+    { Id: 3, Author: 'Matt Barbier', Text: 'Title3' },
+
+];
+
+//class CommentBox extends React.Component {
+//    render() {
+//        return (
+//            <div className="commentBox">
+//                <h1>Comments</h1>
+//                <CommentList data={this.props.data} />
+//                <CommentForm />
+//            </div>)
+//    }
+//}
+
+class CommentBox extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { data: [] };
+    }
+    componentDidMount() {
+        this.loadCommentsFromServer();
+        window.setInterval(
+            () => this.loadCommentsFromServer(),
+            this.props.pollInterval,
+        );
+    }
+    loadCommentsFromServer() {
+        const xhr = new XMLHttpRequest();
+        xhr.open('get', this.props.url, true);
+        xhr.onload = () => {
+            const data = JSON.parse(xhr.responseText);
+            this.setState({ data: data });
+        };
+        xhr.send();
+    }    
+    render() {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList data={this.state.data} />
+                <CommentForm />
+            </div>
+        );
+    }
+}
+
+
+
+class CommentList extends React.Component {
+    render() {
+        const commentNodes = this.props.data.map(comment => (
+            <Comment author={comment.Author} key={comment.Id}>
+                {comment.Text}
+            </Comment>
+        ));
+
+        return (
+
+            <div className="commentList">{commentNodes}</div>
+            
+        );
+    }
+}
+
+class CommentForm extends React.Component {
+    render() {
+        return (
+            
+            <div className="commentForm">Hello world! I am a Comment Form!</div>);
+    }
+}
+
+class Comment extends React.Component {
+    render() {
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">{this.props.author}</h2>
+                {this.props.children.toString()}
+            </div>
+            )
+    }
+}
+
+//ReactDOM.render(<CommentBox data={data} />, document.getElementById('content'));
+ReactDOM.render(
+    <CommentBox url="/comments" pollInterval={10000} />,
+    document.getElementById('content'),
+);
