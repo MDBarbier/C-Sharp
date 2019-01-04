8d58a71f2e3a9d2c244a203cb233e75b83445f50
matt barbier <matthewdavidbarbier@gmail.com>
2019-01-04 15:47:14 +0000

    added recursion demo

---
+++ RecursionDemo/RecursionDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RecursionExample
+{
+    internal class RecursionExample
+    {
+        static void Main()
+        {
+            var items = new List<string> { "Node1", "Node2", "Node3" };
+
+            string output = BuildRecursionTree("", items, 0);
+
+            // Result: Node1 <- Node2 <- Node3 <-.
+            Console.Write(output);
+        }
+
+        static string BuildRecursionTree(string value, IList<string> items, int index)
+        {
+            // "index" variable value checking helps call recursive function finite number of times.
+            if (items.Count == index)
+            {
+                return value;
+            }
+
+            // Add node.
+            value += " " + items[index] + " <-";
+
+            // Actual recursion: call function itself once more time.
+            return BuildRecursionTree(value, items, index + 1);
+        }
+    }
+}
