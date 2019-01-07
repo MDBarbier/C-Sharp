7773353b25836ed67ac979c9ec93f6f0b594010f
matt barbier <matthewdavidbarbier@gmail.com>
2019-01-07 11:20:56 +0000

    added anonymous delegate demo

---
+++ anon-delegate-test/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+delegate void Procedure();
+
+
+namespace anon_delegate_test
+{
+    class Program
+    {
+        static Procedure someProcs = null;
+
+        private static void AddProc()
+        {
+            int variable = 100;
+
+            someProcs += new Procedure(delegate
+            {
+                Console.WriteLine(variable);
+            });
+        }
+
+        static void Main()
+        {
+            someProcs += new Procedure(delegate { Console.WriteLine("test"); });
+            AddProc();
+            someProcs();
+            Console.ReadKey();
+        }
+    }
+}
