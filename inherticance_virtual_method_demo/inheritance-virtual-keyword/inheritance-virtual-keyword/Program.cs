07f5b9eef737fa6836aa3880ec5e143e512ae09a
matt barbier <matthewdavidbarbier@gmail.com>
2019-01-27 14:01:56 +0000

    virtual method inheritance demo

---
+++ inherticance_virtual_method_demo/inheritance-virtual-keyword/inheritance-virtual-keyword/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance_virtual_keyword
+{
+    public class Resource : IDisposable
+    {
+        private bool _isClosed = false;    // good programming practice initialise, although default
+
+        protected virtual void Close()
+        {
+            Console.WriteLine("Base resource closer called!");
+        }
+
+        ~Resource()
+        {
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            if (!_isClosed)
+            {
+                Console.WriteLine("Disposing resource and calling the Close() method...");
+                _isClosed = true;
+                Close();
+            }
+        }
+    }
+
+    public class AnotherTypeOfResource : Resource
+    {
+        protected override void Close()
+        {
+            Console.WriteLine("Another type of resource closer called!");
+        }
+    }
+
+    public class VirtualMethodDemo
+    {
+        public static void Main()
+        {
+            Resource res = new Resource();
+            AnotherTypeOfResource res2 = new AnotherTypeOfResource();
+
+            res.Dispose();  // Resource.Close() will be called.
+            res2.Dispose(); // Even though Dispose() is part of the Resource class, 
+                            // the Resource class will call AnotherTypeOfResource.Close()!
+        }
+    }
+
+
+}
