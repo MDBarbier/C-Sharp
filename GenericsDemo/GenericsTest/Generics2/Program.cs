8e1772691344118bc10bce25ea5d62c2b5bf8cb4
matt barbier <matthewdavidbarbier@gmail.com>
2019-01-27 15:55:31 +0000

    interface demo

---
+++ GenericsDemo/GenericsTest/Generics2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericObjectContainer<int> container = new GenericObjectContainer<int>(25);
+            GenericObjectContainer<int> container2 = new GenericObjectContainer<int>(5);
+            Console.WriteLine(container.getObject() + container2.getObject());
+
+            Console.ReadKey(); // wait for user to press any key, so we could see results
+        }
+    }
+
+    public class GenericObjectContainer<T>
+    {
+        private T _obj;
+
+        public GenericObjectContainer(T obj)
+        {
+            this._obj = obj;
+        }
+
+        public T getObject()
+        {
+            return this._obj;
+        }
+    }
+}
---
+++ interface_demo/interface_demo/interface_demo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interface_demo
+{
+    class Program
+    {
+        interface IShape
+        {
+            double X { get; set; }
+            double Y { get; set; }
+            void Draw();
+            //void Render(); //if we uncomment this line the Square class will not compile because it must implement the method Render()
+        }
+
+        class Square : IShape
+        {
+            private double _mX, _mY;
+
+            public void Draw()
+            {
+                Console.WriteLine("I'm a square!");
+            }
+
+            public double X
+            {
+                set { _mX = value; }
+                get { return _mX; }
+            }
+
+            public double Y
+            {
+                set { _mY = value; }
+                get { return _mY; }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Square sq = new Square();
+            sq.Draw();
+        }
+    }
+}
