c3e23c2dc515a7864aa42c2cf0959a908e841e23
matt barbier <matthewdavidbarbier@gmail.com>
2019-01-27 16:36:47 +0000

    marchalling demo

---
+++ marshalling/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+public class ConsoleApp1
+{
+    [DllImport("kernel32.dll")]
+    static extern bool GetSystemTimes(out long idleTime, out long kernelTime, out long userTime);
+
+    public static void Main()
+    {
+        long idleTime, kernelTime, userTime;
+
+        GetSystemTimes(out idleTime, out kernelTime, out userTime);
+        Console.WriteLine("Your CPU(s) have been idle for: " + (new TimeSpan(idleTime)).ToString());
+        Console.ReadKey();
+    }
+}
