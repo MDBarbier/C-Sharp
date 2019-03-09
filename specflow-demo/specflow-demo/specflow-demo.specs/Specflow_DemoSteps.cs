bb516e8684a7aab710640c51e6b441fd6848be1e
matt barbier <matthewdavidbarbier@gmail.com>
2019-03-09 11:37:11 +0000

    spec flow demo

---
+++ specflow-demo/specflow-demo/specflow-demo.specs/Specflow_DemoSteps.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using ClassLibrary1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace specflow_demo.specs
+{
+    [Binding]
+    public class Specflow_DemoSteps
+    {
+        private int result;
+        Calculator c = new Calculator();
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int number)
+        {
+            c.FirstNumber = number;
+        }
+        
+        [Given(@"I have also entered (.*) into the calculator")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculator(int number)
+        {
+            c.SecondNumber = number;
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            result = c.Add();
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
