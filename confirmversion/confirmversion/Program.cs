c0f53a167a0f12724978334379f1b88f7dbfd180
MDBarbier <matthew-7.7@hotmail.com>
2019-03-31 07:29:31 +0100

    added confirm version core project

---
+++ confirmversion/confirmversion/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace confirmversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic targetFrameworkAttribute = Assembly.GetExecutingAssembly()
+                .GetCustomAttributes(typeof(System.Runtime.Versioning.TargetFrameworkAttribute), false)
+                .SingleOrDefault();
+            string framework = targetFrameworkAttribute.FrameworkName;
+            
+            Console.WriteLine($".Net Core version {framework.Split('=')[1]} is installed and working.");
+        }
+    }
+}
