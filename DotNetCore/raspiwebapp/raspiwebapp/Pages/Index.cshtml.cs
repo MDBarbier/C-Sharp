345253d06ec641351251c4e666365bc5f4a3de0c
MDBarbier <matthew-7.7@hotmail.com>
2019-03-31 08:43:13 +0100

    added raspiwebapp

---
+++ DotNetCore/raspiwebapp/raspiwebapp/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace raspiwebapp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            dynamic targetFrameworkAttribute = Assembly.GetExecutingAssembly()
+                .GetCustomAttributes(typeof(System.Runtime.Versioning.TargetFrameworkAttribute), false)
+                .SingleOrDefault();
+            string framework = targetFrameworkAttribute.FrameworkName;
+
+            ViewData["Version"] = framework;
+            ViewData["Hostname"] = Dns.GetHostName();
+        }
+    }
+}
