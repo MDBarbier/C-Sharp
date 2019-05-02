80f4466009ef651c3a51acc26ee6a161bbe5493e
MDBarbier <matthew-7.7@hotmail.com>
2019-05-02 19:09:47 +0100

    added wcf demo and soap consume demo

---
+++ WCF/WcfServiceLibrary1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+
+            var reply = client.GetData(5);
+
+            Console.WriteLine("Reply from web service: " + reply);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ soaptest/soaptest/Program.cs
@@ -0,0 +1,21 @@
+﻿using ServiceReference1;
+using System;
+using System.Threading.Tasks;
+
+namespace soaptest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            CalculatorSoapClient soapClient = new CalculatorSoapClient(CalculatorSoapClient.EndpointConfiguration.CalculatorSoap);
+
+            var message = System.Configuration.ConfigurationManager.AppSettings["test"];
+
+            var answer = await soapClient.AddAsync(5, 5);
+
+            Console.WriteLine("The answer is: " + answer);
+        }
+    }
+}
