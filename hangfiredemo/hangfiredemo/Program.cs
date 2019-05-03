2d0833f14b1952c3188e20bf41151fa0cfe0f81d
Matt Barbier <matt.barbier@os.uk>
2019-05-03 15:31:18 +0100

    added hangfire demo

---
+++ hangfiredemo/hangfiredemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Hangfire;
+using Hangfire.SqlServer;
+
+namespace hangfiredemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hangfire demo application started....");
+
+            GlobalConfiguration.Configuration
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                .UseColouredConsoleLogProvider()
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UseSqlServerStorage("Database=Hangfire.Sample; Integrated Security=True;", new SqlServerStorageOptions
+                {
+                    CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+                    SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+                    QueuePollInterval = TimeSpan.Zero,
+                    UseRecommendedIsolationLevel = true,
+                    UsePageLocksOnDequeue = true,
+                    DisableGlobalLocks = true
+                });
+
+            BackgroundJob.Enqueue(() => Console.WriteLine("Hello, world! Coming to you via a background thread"));
+
+            Console.WriteLine("Main thread execution finished");
+
+            using (var server = new BackgroundJobServer())
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
