3066b3bd7323d91d9a96941781a9a740ca6a2ac3
matt barbier <matthewdavidbarbier@gmail.com>
2019-08-11 16:58:39 +0100

    added docker demo

---
+++ docker-demo/dockerdemos/dockerdemos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dockerdemos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello World! This app is running in a docker container on the {Environment.OSVersion} operating system");
+        }
+    }
+}
---
+++ docker-demo/dockerdemos/readme.txt
@@ -0,0 +1,4 @@
+To run the program container in docker, execute these commands in PowerShell:
+
+\docker-demo\dockerdemos> docker build -t dockerdemos .
+docker-demo\dockerdemos> docker run dockerdemos
