832b765dad7eadc92cf73ca08bddddca97b4827f
Matt Barbier <matt.barbier@os.uk>
2019-09-11 08:44:12 +0100

    added date parsing function, work in progress

---
+++ DateParsing/DateParsing/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DateParsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string weirdDate = "31:12:2019 00:00:00";
+            
+
+            string formatString = "dd':'MM':'yyyy' 'HH':'mm':'ss";            
+            var parsedDate = DateTime.ParseExact(weirdDate, formatString, null);
+
+            Console.WriteLine($"The parsed date is {parsedDate}");
+            Console.ReadLine();
+        }
+
+        public static DateTime ParseDateFromStringWithFormat(string datetime, string format)
+        {
+            if (DateTime.TryParseExact(datetime, format, null, null, out DateTime result))
+            {
+
+            }
+        }
+    }
+}
