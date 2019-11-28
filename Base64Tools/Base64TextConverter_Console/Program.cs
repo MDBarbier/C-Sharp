8d220381c0649c98fef0f8835d790237a37d7e8b
Matt Barbier <Matt.Barbier@os.uk>
2019-11-28 11:35:13 +0000

    added base64 decode program

---
+++ Base64Tools/Base64TextConverter_Console/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace Base64TextConverter_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Paste the base64:");
+
+            var b64 = Console.ReadLine();
+
+            var decoded = DecodeBase64(b64);
+
+            Console.WriteLine(decoded);
+        }
+
+        public static string DecodeBase64(string base64encoded)
+        {
+            string base64Decoded;
+            byte[] data = Convert.FromBase64String(base64encoded);
+            base64Decoded = ASCIIEncoding.ASCII.GetString(data);
+            return base64Decoded;
+        }
+    }
+}
---
+++ Base64Tools/Base64Tools/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Base64Tools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
