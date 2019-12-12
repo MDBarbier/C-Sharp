828fc1ae02e4371499228c476ff2ff96b80785ce
Matt Barbier <Matt.Barbier@os.uk>
2019-12-12 15:16:39 +0000

    added linq demo project

---
+++ LINQdemos/BasicQuery/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BasicQuery
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            IEnumerable<string> fruits = new List<string>()
+            {
+                "Apple",
+                "Orange",
+                "Pineapple",
+                "Star",
+                "Melon",
+                "Banana"
+            };
+
+            BasicQuery(fruits);
+            MethodSyntaxQuery(fruits);
+        }
+
+        private static void BasicQuery(IEnumerable<string> fruits)
+        {
+            // Define the query using query syntax
+            var fruitQuery =
+                from fruit in fruits
+                where fruit.Contains('a')
+                select fruit;
+
+            // Execute the query
+            foreach (var i in fruitQuery)
+            {
+                Console.Write(i + " ");
+            }            
+        }
+
+        private static void MethodSyntaxQuery(IEnumerable<string> fruits)
+        {
+            //Define a query using method syntax
+            var orderedFruits = fruits.Where(fruit => fruit.Contains('a')).OrderBy(fruit => fruit);
+
+            Console.WriteLine();
+
+            // Execute the query.
+            foreach (var i in orderedFruits)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
