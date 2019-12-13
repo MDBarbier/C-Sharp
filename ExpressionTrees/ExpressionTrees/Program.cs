63ae5f1b909ddb3baadbccd50a26234dc3a69d34
Matt Barbier <Matt.Barbier@os.uk>
2019-12-13 11:11:54 +0000

    Added expression tree demo

---
+++ ExpressionTrees/ExpressionTrees/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace ExpressionTrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParameterExpression pe = Expression.Parameter(typeof(Student), "s");
+
+            MemberExpression me = Expression.Property(pe, "Age");
+
+            ConstantExpression constant = Expression.Constant(18, typeof(int));
+
+            BinaryExpression body = Expression.GreaterThanOrEqual(me, constant);
+
+            var ExpressionTree = Expression.Lambda<Func<Student, bool>>(body, new[] { pe });
+
+            Console.WriteLine("Expression Tree: {0}", ExpressionTree);
+
+            Console.WriteLine("Expression Tree Body: {0}", ExpressionTree.Body);
+
+            Console.WriteLine("Number of Parameters in Expression Tree: {0}",
+                ExpressionTree.Parameters.Count);
+
+            Console.WriteLine("Parameters in Expression Tree: {0}", ExpressionTree.Parameters[0]);
+        }
+    }
+
+    class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+    }
+}
