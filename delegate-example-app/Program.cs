e70b78eebfe30c383df9b692c5ac2d5839afab93
Matt Barbier <Matt.Barbier@os.uk>
2019-12-16 15:59:16 +0000

    added more delegate examples

---
+++ delegate-example-app/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace delegate_example_app
+{
+
+    /// <summary>
+    /// A class to define a person
+    /// </summary>
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    internal class Program
+    {
+        //Our delegate
+        public delegate bool FilterDelegate(Person p);
+
+        private static void Main()
+        {
+
+            //Create 4 Person objects
+            var p1 = new Person() { Name = "John", Age = 41 };
+            var p2 = new Person() { Name = "Jane", Age = 69 };
+            var p3 = new Person() { Name = "Jake", Age = 12 };
+            var p4 = new Person() { Name = "Jessie", Age = 25 };
+
+            //Create a list of Person objects and fill it
+            var people = new List<Person>() { p1, p2, p3, p4 };
+
+            //Invoke DisplayPeople using appropriate delegate
+            DisplayPeople("Children:", people, IsChild);
+            DisplayPeople("Adults:", people, IsAdult);
+            DisplayPeople("Seniors:", people, IsSenior);
+
+            Console.Read();
+        }
+
+        /// <summary>
+        /// A method to filter out the people you need
+        /// </summary>
+        /// <param name="people">A list of people</param>
+        /// <param name="filter">A filter</param>
+        /// <param name="title">Text to display></param>
+        /// <returns>A filtered list</returns>
+        private static void DisplayPeople(string title, IEnumerable<Person> people, FilterDelegate filter)
+        {
+            Console.WriteLine(title);
+
+            foreach (var p in people)
+            {
+                if (filter(p))
+                {
+                    Console.WriteLine("{0}, {1} years old", p.Name, p.Age);
+                }
+            }
+
+            Console.Write("\n\n");
+        }
+
+        //==========FILTERS===================
+        private static bool IsChild(Person p)
+        {
+            return p.Age < 18;
+        }
+
+        private static bool IsAdult(Person p)
+        {
+            return p.Age >= 18;
+        }
+
+        private static bool IsSenior(Person p)
+        {
+            return p.Age >= 65;
+        }
+    }
+}
---
+++ event-demo-2/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace event_demo_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var posm = new PizzaOrderingSystemMethod();
+
+            var pos = new PizzaOrderingSystem(
+                DiscountPolicyDelegates.DiscountAllThePizzas());
+
+            PizzaOrder po = new PizzaOrder();
+            po.Pizzas = new List<Pizza>();
+            po.Pizzas.Add(new Pizza() {Price = 15.99m, CrustType = CrustType.Thin});
+            po.Pizzas.Add(new Pizza() { Price = 16.98m, CrustType = CrustType.Stuffed });
+            po.Pizzas.Add(new Pizza() { Price = 15.99m, CrustType = CrustType.Thin });
+
+
+            Console.WriteLine(pos.ComputePrice(po));
+
+           
+
+        }
+    }
+
+    public delegate decimal DiscountPolicy(PizzaOrder order);
+
+    class PizzaOrderingSystemMethod
+    {
+        private readonly DiscountPoliciesMethods _discountPoliciesMethods;
+        public PizzaOrderingSystemMethod()
+        {
+            _discountPoliciesMethods = new DiscountPoliciesMethods();
+        }
+        public decimal ComputePrice(PizzaOrder order)
+        {
+            decimal total = order.Pizzas.Sum(p => p.Price);
+
+            decimal[] discounts = new[] {
+                _discountPoliciesMethods.BuyOneGetOneFree(order),
+                _discountPoliciesMethods.FivePercentOffMoreThanFiftyDollars(order),
+                _discountPoliciesMethods.FiveDollarsOffStuffedCrust(order),
+            };
+
+            decimal bestDiscount = discounts.Max(discount => discount);
+            total = total - bestDiscount;
+            return total;
+        }
+    }
+    public class PizzaOrderingSystem
+    {
+        readonly DiscountPolicy _discountPolicy;
+
+        public PizzaOrderingSystem(DiscountPolicy discountPolicy)
+        {
+            _discountPolicy = discountPolicy;
+        }
+
+        public decimal ComputePrice(PizzaOrder order)
+        {
+            decimal nonDiscounted = order.Pizzas.Sum(p => p.Price);
+            decimal discountedValue = _discountPolicy(order);
+            return nonDiscounted - discountedValue;
+        }
+    }
+
+    public static class DiscountPolicyDelegates
+    {
+        public static decimal BuyOneGetOneFree(PizzaOrder order)
+        {
+            var pizzas = order.Pizzas;
+            if (pizzas.Count < 2)
+            {
+                return 0m;
+            }
+            return pizzas.Min(p => p.Price);
+        }
+        public static decimal FivePercentOffMoreThanFiftyDollars(PizzaOrder order)
+        {
+            decimal nonDiscounted = order.Pizzas.Sum(p => p.Price);
+            return nonDiscounted >= 50 ? nonDiscounted * 0.05m : 0M;
+        }
+        public static decimal FiveDollarsOffStuffedCrust(PizzaOrder order)
+        {
+            return order.Pizzas.Sum(p => p.CrustType == CrustType.Stuffed ? 5m : 0m);
+        }
+        public static DiscountPolicy CreateBest(params DiscountPolicy[] policies)
+        {
+            return order => policies.Max(policy => policy.Invoke(order));
+        }
+
+        public static DiscountPolicy DiscountAllThePizzas()
+        {
+            DiscountPolicy best = CreateBest(
+                BuyOneGetOneFree,
+                FivePercentOffMoreThanFiftyDollars,
+                FiveDollarsOffStuffedCrust);
+            return best;
+        }
+    }
+
+    public class PizzaOrder
+    {
+        public List<Pizza> Pizzas { get; internal set; }
+    }
+
+    public class Pizza
+    {
+        public decimal Price { get; set; }
+        public CrustType CrustType { get; set; }
+    }
+
+    public enum CrustType
+    {
+        Stuffed, Thin
+    }
+
+    public class DiscountPoliciesMethods
+    {
+        public decimal BuyOneGetOneFree(PizzaOrder order)
+        {
+            var pizzas = order.Pizzas;
+            if (pizzas.Count < 2)
+            {
+                return 0m;
+            }
+            return pizzas.Min(p => p.Price);
+        }
+        public decimal FivePercentOffMoreThanFiftyDollars(PizzaOrder order)
+        {
+            decimal nonDiscounted = order.Pizzas.Sum(p => p.Price);
+            return nonDiscounted >= 50 ? nonDiscounted * 0.05m : 0M;
+
+        }
+        public decimal FiveDollarsOffStuffedCrust(PizzaOrder order)
+        {
+            return order.Pizzas.Sum(p => p.CrustType == CrustType.Stuffed ? 5m : 0m);
+        }
+    }
+}
