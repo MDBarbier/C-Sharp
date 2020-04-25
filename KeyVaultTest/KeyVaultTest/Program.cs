ed9871a3df8065cddd9516c0e3c6803955558358
mdbarbier <matthewdavidbarbier@gmail.com>
2020-04-25 14:05:21 +0100

    added keyvault test app

---
+++ KeyVaultTest/KeyVaultTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace KeyVaultTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting KeyVault test app");
+
+            AzureKeyVaultLib.AzureKeyVaultManager akvm = new AzureKeyVaultLib.AzureKeyVaultManager();
+
+            var secret = akvm.GetSecretValue("testSecret1");
+
+            Console.WriteLine($"The secret value is: {secret}");
+
+            var allSecretProps = akvm.GetAllSecretProperties();
+
+            foreach (var secretProps in allSecretProps)
+            {
+                Console.WriteLine($"Secret name: {secretProps.Name}, created on: {secretProps.CreatedOn}");
+            }
+        }
+    }
+}
