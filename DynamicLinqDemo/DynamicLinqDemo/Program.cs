c7cae9e888dcf4f61fa5d3d06b0c8efa2a36601c
matt barbier <mdbarbier@protonmail.com>
2020-10-09 11:53:06 +0100

    Refactored Dynamic linq demo

--- DynamicLinqDemo/DynamicLinqDemo/Program.cs
+++ DynamicLinqDemo/DynamicLinqDemo/Program.cs
@@ -9,44 +9,33 @@ namespace DynamicLinqDemo
 
     {
 
-        static void Main(string[] args)
+        static void Main()
 
         {
-
-            List<TestClass> data = new List<TestClass>();
-
-            data.Add(new TestClass() { Age = 50, Name = "Jon" });
-
-            data.Add(new TestClass() { Age = 37, Name = "Zon" });
-
-            data.Add(new TestClass() { Age = 28, Name = "Ron" });
-
-            data.Add(new TestClass() { Age = 85, Name = "Don" });
-
-
+            List<TestClass> data = FillTestData();
 
             var queryable = data.AsQueryable();
 
-            string predicate = "Name == \"Zon\"";
+            string predicate = "Name == \"Zon\" && Age == 37";
 
-            var results = DoTheThing(queryable, predicate);
-
-
+            var results = queryable.Where(predicate).ToList();
 
             foreach (var result in results)
-
             {
-
                 Console.WriteLine($"Name: {result.Name}, Age: {result.Age}");
-
             }
-
         }
 
+        private static List<TestClass> FillTestData()
+        {
+            List<TestClass> data = new List<TestClass>();
 
-
-        public static List<TestClass> DoTheThing(IQueryable<TestClass> data, string predicate) => data.Where(predicate).ToList();
-
+            data.Add(new TestClass() { Age = 50, Name = "Jon" });
+            data.Add(new TestClass() { Age = 37, Name = "Zon" });
+            data.Add(new TestClass() { Age = 28, Name = "Ron" });
+            data.Add(new TestClass() { Age = 85, Name = "Don" });
+            return data;
+        }
     }
 
     public class TestClass
