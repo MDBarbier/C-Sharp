65ff307cadb37fc5875ef0ceb572826a9f9bf0df
matt barbier <mdbarbier@protonmail.com>
2020-11-22 14:53:06 +0000

    added dotnet 5 demo

---
+++ dotnet5test1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace dotnet5test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var dotnetversion = Environment.Version;
+            Console.WriteLine($"This app is running on {dotnetversion}");
+        }
+    }
+}
