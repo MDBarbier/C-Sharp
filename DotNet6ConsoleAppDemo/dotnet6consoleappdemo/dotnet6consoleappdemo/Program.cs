03b7d340e85e2969eb8459bd1eaa60e65c816f5a
matt barbier <mdbarbier@protonmail.com>
2021-11-12 12:03:43 +0000

    Added demo of a console app in dotnet 6

---
+++ DotNet6ConsoleAppDemo/dotnet6consoleappdemo/dotnet6consoleappdemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+#region Main
+
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+await LongRunningProcess(3000);
+Console.WriteLine(GetMessage("Matt"));
+Console.WriteLine($"Finished in {stopwatch.Elapsed}");
+stopwatch.Stop();
+
+#endregion
+
+
+#region Local functions
+
+string GetMessage(string name) => $"Hello {name}";
+
+async Task LongRunningProcess(int delay)
+{
+    await Task.Delay(delay);
+}
+
+#endregion
